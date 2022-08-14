05d4a095bb64a7619d1a81c9d4ecb5206bec9b45
André Maiolini <andre.maiolini@hotmail.com>
2022-08-14 20:24:52 -0300

    Inserção de dados no banco de dados!

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,38 +2,44 @@ package app;
 
 import db.DB;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection conn;
-        Statement st = null;
-        ResultSet rs = null;
+        PreparedStatement st = null;
 
         try {
 
             conn = DB.getConnection();
 
-            st = conn.createStatement();
-            rs = st.executeQuery("select * from department");
+            st = conn.prepareStatement("INSERT INTO seller"
+                                         + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+                                         + "VALUES "
+                                         + "(?,?,?,?,?)");
 
-            while(rs.next()) {
-                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-            }
+            st.setString(1,"Carl Purple");
+            st.setString(2,"carl@gmail.com");
+            st.setDate(3,new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+            st.setDouble(4,3000.00);
+            st.setInt(5,4);
 
-        } catch (SQLException e) {
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
+
+        } catch (SQLException | ParseException e) {
 
             e.printStackTrace();
 
         } finally {
 
             DB.closeStatement(st);
-            DB.closeResultSet(rs);
             DB.closeConnection();
 
         }
